0205a2636278b2863006b0d58922e7f98f09dfc3
Guillaume Meigniez <guillaume.meigniez@gmail.com>
2023-08-02 17:44:43 +0200

    [VERSION] Add version number in UI. closes #4

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,7 +1,11 @@
 import styles from './footer.module.scss';
+const { version } = require('../../../package.json');
 
 export default function Footer() {
+
     return <div className={styles.footer}>
         <a className="bro-link" target="_blank" href="http://www.guillaumemeigniez.me">Guillaume MMM</a>
+
+        <div><span className="bro-muted">{version}</span></div>
     </div>
 }
