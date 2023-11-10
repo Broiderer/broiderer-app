a143ab1b957272b14c1244ab6833a173e1d74506
Guillaume Meigniez <guillaume.meigniez@gmail.com>
2023-11-10 17:19:01 +0100

    [VISUALIZE] add drag & zoom to the canvas. closes #13

--- app/components/editor/editor.tsx
+++ app/components/editor/editor.tsx
@@ -1,12 +1,16 @@
 'use client'
 
-import { useEffect, useRef } from 'react'
+import { MouseEvent, WheelEvent, useEffect, useRef, useState } from 'react'
 import * as paper from 'paper'
 import styles from './editro.module.scss'
+import { Point } from 'paper/dist/paper-core'
 
 paper.install(document)
+const ZOOM_FACTOR = 1.02
 
 export default function Editor() {
+  const [isDragging, setIsDragging] = useState<boolean>(false)
+
   const paperRef = useRef<HTMLCanvasElement>(null)
 
   useEffect(() => {
@@ -15,19 +19,86 @@ export default function Editor() {
       paper.view.autoUpdate = true
       console.log('install')
 
-      var path = new paper.Path()
-      // Give the stroke a color
-      path.strokeColor = 'black' as any
-      var start = new paper.Point(100, 100)
-      // Move to start and draw a line from there
-      path.moveTo(start)
-      // Note that the plus operator on Point objects does not work
-      // in JavaScript. Instead, we need to call the add() function:
-      path.lineTo(start.add([1000, 1050]))
-      // Draw the view now:
-      paper.view.requestUpdate()
+      initFakeScene()
     }
-  }, [paperRef.current])
+  }, [paperRef])
 
-  return <canvas className={styles['paper-canvas']} ref={paperRef}></canvas>
+  function initFakeScene() {
+    var path = new paper.Path()
+
+    new paper.Path.Circle({
+      center: paper.view.center,
+      radius: 100,
+      fillColor: 'red',
+    })
+    // Give the stroke a color
+    path.strokeColor = 'black' as any
+    var start = new paper.Point(100, 100)
+    // Move to start and draw a line from there
+    path.moveTo(start)
+    // Note that the plus operator on Point objects does not work
+    // in JavaScript. Instead, we need to call the add() function:
+    path.lineTo(start.add([1000, 1050]))
+    // Draw the view now:
+    paper.view.requestUpdate()
+  }
+
+  function wheelHandler(e: WheelEvent<HTMLCanvasElement>) {
+    // Store previous view state.
+    const oldZoom = paper.view.zoom
+    const oldCenter = paper.view.center
+
+    // Get mouse position.
+    // It needs to be converted into project coordinates system.
+    const mousePosition = paper.view.viewToProject(
+      new Point(e.clientX, e.clientY)
+    )
+
+    // Update view zoom.
+    var newZoom = e.deltaY < 0 ? oldZoom * ZOOM_FACTOR : oldZoom / ZOOM_FACTOR
+    paper.view.zoom = newZoom
+
+    // Update view position.
+    paper.view.center = new Point(
+      oldCenter.x + (mousePosition.x - oldCenter.x) * (1 - oldZoom / newZoom),
+      oldCenter.y + (mousePosition.y - oldCenter.y) * (1 - oldZoom / newZoom)
+    )
+
+    paper.view.requestUpdate()
+  }
+
+  function mouseDownHandler(e: MouseEvent<HTMLCanvasElement>) {
+    setIsDragging(true)
+  }
+
+  function mouseUpHandler(e: MouseEvent<HTMLCanvasElement>) {
+    setIsDragging(false)
+  }
+
+  function mouseOutHandler(e: MouseEvent<HTMLCanvasElement>) {
+    setIsDragging(false)
+  }
+
+  function mouseMoveHandler(e: MouseEvent<HTMLCanvasElement>) {
+    if (isDragging) {
+      paper.view.center = new Point(
+        paper.view.center.x - e.movementX / paper.view.zoom,
+        paper.view.center.y - e.movementY / paper.view.zoom
+      )
+    }
+  }
+
+  return (
+    <canvas
+      className={`${styles['paper-canvas']} ${
+        isDragging ? styles['dragging'] : ''
+      }`}
+      ref={paperRef}
+      onWheel={wheelHandler}
+      onMouseDown={mouseDownHandler}
+      onMouseUp={mouseUpHandler}
+      onMouseOut={mouseOutHandler}
+      onMouseMove={mouseMoveHandler}
+    ></canvas>
+  )
 }
