2cf50468d771c484fc1df9f8054bcd2900c83403
Guillaume Meigniez <guillaume.meigniez@gmail.com>
2023-11-24 12:15:38 +0100

    [VISUALIZE] Clean svg on import. closes #21

--- app/components/editor/editor-sidebar/editor-sidebar-forms/editor-sidebar-form-svg/editor-sidebar-form-svg.tsx
+++ app/components/editor/editor-sidebar/editor-sidebar-forms/editor-sidebar-form-svg/editor-sidebar-form-svg.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent } from 'react'
 import { EditorSettings } from '../../../editor'
+import * as svgo from 'svgo'
 
 export default function EditorSidebarFromSvg({
   updateImportSettings,
@@ -14,9 +15,62 @@ export default function EditorSidebarFromSvg({
       const reader = new FileReader()
 
       reader.onload = (e) => {
-        const svgContent = e.target?.result
+        const svgContent = e.target?.result as string
         if (svgContent) {
-          updateImportSettings({ initialSvg: svgContent as string })
+          const optimizedData = svgo.optimize(svgContent, {
+            js2svg: { indent: 2, pretty: true },
+            plugins: [
+              'cleanupAttrs',
+              'cleanupEnableBackground',
+              'cleanupIds',
+              { name: 'cleanupListOfValues', params: { convertToPx: true } },
+              { name: 'cleanupNumericValues', params: { convertToPx: true } },
+              'collapseGroups',
+              {
+                name: 'convertColors',
+                params: { names2hex: true, rgb2hex: true },
+              },
+              {
+                name: 'convertPathData',
+                params: {
+                  applyTransforms: true,
+                  applyTransformsStroked: true,
+                  convertToZ: true,
+                  removeUseless: true,
+                },
+              },
+              { name: 'convertShapeToPath', params: { convertArcs: true } },
+              'convertStyleToAttrs',
+              'inlineStyles',
+              'removeComments',
+              'removeDesc',
+              'removeDoctype',
+              'removeDimensions',
+              'removeEditorsNSData',
+              'removeEmptyContainers',
+              'removeEmptyText',
+              'removeHiddenElems',
+              'removeMetadata',
+              'removeNonInheritableGroupAttrs',
+              'removeOffCanvasPaths',
+              'removeRasterImages',
+              'removeScriptElement',
+              'removeStyleElement',
+              'removeUnknownsAndDefaults',
+              'removeUnusedNS',
+              'removeUselessDefs',
+              {
+                name: 'removeAttrs',
+                params: {
+                  attrs: ['*:stroke:*', '*:stroke-width:*'],
+                },
+              },
+            ],
+          }).data
+
+          updateImportSettings({
+            initialSvg: optimizedData,
+          })
         }
       }
 
