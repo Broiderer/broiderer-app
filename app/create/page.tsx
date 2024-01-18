5c280cf6ed4ec2d2baed824622eddf2e080b95f5
Guillaume Meigniez <guillaume.meigniez@gmail.com>
2024-01-18 20:28:42 +0100

    [NEXT] Dynamically load Editor to avoid paperjs window onload.

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,13 +1,17 @@
 'use client'
 
-import Editor from '../components/editor/editor'
+import dynamic from 'next/dynamic'
 import styles from './styles.module.scss'
+
+const DynamicEditor = dynamic(() => import('../components/editor/editor'), {
+  ssr: false,
+})
 
 export default function Create() {
   return (
     <main className={styles.create}>
       <div className={styles['create-content']}>
-        <Editor></Editor>
+        <DynamicEditor></DynamicEditor>
       </div>
     </main>
   )
