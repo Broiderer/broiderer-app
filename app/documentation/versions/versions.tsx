e4360818f230faa24ad3563e77596dea8c52f504
Guillaume Meigniez <guillaume.meigniez@gmail.com>
2024-01-19 20:57:53 +0100

    [DOC] Add dates and version 1.1.0 details.

--- app/documentation/versions/versions.tsx
+++ app/documentation/versions/versions.tsx
@@ -2,13 +2,24 @@ import styles from './versions.module.scss'
 
 type Version = {
   name: string
+  date: string
   tasks: (JSX.Element | string)[]
 }
 
 export default function Versions() {
   const versions: Version[] = [
     {
+      name: '1.1.0',
+      date: '19/01/2024',
+      tasks: [
+        'Create the version page in the doc',
+        'Re-position the imported shapes on the top right corner to avoid mis-interpretation of the bounds on export',
+        'Allow for a different filling setup for each shape',
+      ],
+    },
+    {
       name: '1.0.1 - first release',
+      date: '17/01/2024',
       tasks: ['Editor page with svg import and basic customization.'],
     },
   ]
@@ -21,9 +32,12 @@ export default function Versions() {
           <h3 className={`${styles['version-name']} bro-title-3`}>
             {version.name}
           </h3>
-          {version.tasks.map((task, j) => (
-            <p key={j}>✅ {task}</p>
-          ))}
+          <p className={styles['version-date']}>{version.date}</p>
+          <div className={styles['version-tasks']}>
+            {version.tasks.map((task, j) => (
+              <p key={j}>✅ {task}</p>
+            ))}
+          </div>
         </div>
       ))}
     </>
